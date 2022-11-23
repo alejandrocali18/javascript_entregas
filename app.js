e961974c16bc0cf0ac79965e5a1630d81525cd99
Alejandro <alejandrocumo18@gmail.com>
2022-11-23 16:38:15 -0300

    creacion de repositorio para JS

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+let food= prompt("")
+
+switch (){
+    case "":
+    console.log("")
+    break;
+    case "":
+        console.log("")
+        break;
+        case "":
+            console.log("")
+            break;
+            default:
+                console.log("")
+                break;
+}
+*/
+
+
+/*ciclo for*/
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`Me salio y no rompi nada I es: ${i}`);
+}
+
+/*Condicional*/
+
+let efectivo = 4000
+let tarjeta = 6000
+let pagar = 11000
+let disponible = efectivo + tarjeta;
+
+if (efectivo > pagar || tarjeta > pagar || disponible >= pagar) {
+    console.log("podemos hacer la compra");
+} else {
+    console.log("gracias vuelva pronto")
+}
