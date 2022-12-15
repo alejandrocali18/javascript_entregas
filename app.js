b8e866875cf4e06ca27d36f3174fabdf7cf07b9d
Alejandro <alejandrocumo18@gmail.com>
2022-12-15 01:45:44 -0300

    vamos con el tiempo justo

--- app.js
+++ app.js
@@ -1,50 +1,42 @@
-/*-------------------primera entrega---------------------------
+/*           VARIABLES                                 */
+let saludo;
+let nombre;
+let apellido;
+let llevar;
+let compra;
+let banana;
+let naranja;
+let manzana;
+let uva;
+let choclo;
+let carrito;
+let venta;
+let despido;
+let inicioCompra;
 
+function bienVenido(){
+    let nombre = prompt("ingrese su nombre");
+    let apellido = prompt("ingrese su apellido");
 
-function mostrarMensaje(){
-    let apellido;
-    let nombre; 
-    let calificacion;
-    let i;
-    
-    
-    
-    alert(
-        "el promedio de: " + " " + nombres + " " + apellido + " " + " es de: " + calificacion)
-    
-    if (calificacion >= 7) {
-        alert("felicidades pasaste")
-    } else {
-        alert("desaprobaste vuelva a intentarlo")
-    }
-    
-    for (let i = 7; i <= 10; i++) {
-        alert(`se aprueba si tu calificacion es de: ${i}`)
-    }
+    alert("bienvenido" + " " + nombre + " " + apellido + " " + "que va a llevar?")
 }
+bienVenido();
 
--------------------------------------------------*/
-
+/*              OBJECT CONSTRUCTOR                                                     */ 
 function Mercaderia(nombre, precio, kilos){
     this.nombre = nombre;
     this.precio = precio;
     this.kilos = kilos;
 }
-
 const mercaderiaUno = new Mercaderia("Naranja", 300, 5);
 const mercaderiaDos = new Mercaderia("Mandarina", 200, 10);
 const mercaderiaTres = new Mercaderia("Uva", 500, 25);
 const mercaderiaCuatro = new Mercaderia("Pera", 250, 50);
 const mercaderiaCinco = new Mercaderia("Coco", 750, 70);
 
-
 console.log(mercaderiaUno);
 console.log(mercaderiaDos);
 console.log(mercaderiaTres);
 console.log(mercaderiaCuatro);
 console.log(mercaderiaCinco);
-
-function compraMercaderia(){
-    compro = prompt(`que vas a comprar?:
-    1 - Naranja`)
-}
+/*              FIN CONSTRUTOR                           */
