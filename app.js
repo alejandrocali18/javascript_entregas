8089e22d2e1fd59c7e96b534c39c3e93900b54c3
Alejandro <alejandrocumo18@gmail.com>
2022-12-16 23:39:18 -0300

    terminamos

--- app.js
+++ app.js
@@ -35,6 +35,49 @@ if(quellevas == "si"){
 } else if(quellevas == "no"){
     alert("gracias por venir")
 }
+while(quellevas != "no"){
+    let mercaderia = prompt("llevar a la bolsa")
+    let precio = 0 
+
+    if(mercaderia == "naranja" || mercaderia == "mandarina" || mercaderia == "uva" || mercaderia == "pera" || mercaderia == "coco"){
+        switch(mercaderia){
+            case "naranja":
+            precio = 50;
+            break;
+            case "mandarina":
+            precio = 70;
+            break;
+            case "uva":
+            precio = 100;
+            break;
+            case "pera":
+            precio = 80;
+            break;
+            case "coco":
+            precio = 130;
+            break;
+            default:
+                break;
+        }
+        let kilos = parseInt(prompt("cuantos kilos quiere?"))
+        bolsa.push({mercaderia, kilos, precio}) 
+    } else{
+        alert("no tenemos mas")
+    }
+    quellevas = prompt("algo mas lleva?")
+    
+    while (quellevas == "no"){
+        alert("gracias por venir")
+        bolsa.forEach((bolsaLista) => {
+            console.log(`mercaderia: ${bolsaLista.mercaderia}, kilos: ${bolsaLista.kilos}, precio es ${bolsaLista.precio} debe
+            ${bolsaLista.kilos * bolsaLista.precio}`)
+        })
+        break;
+    }
+}
+
+const Totalbolsa = bolsa.reduce((acc, el)=> acc + el.precio * el.kilos, 0)
+console.log(`a pagar es: ${Totalbolsa}`)
 
 
 /*              OBJECT CONSTRUCTOR                                                     */ 
