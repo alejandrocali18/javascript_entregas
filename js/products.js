45c8498f08a6c5e275fad33537d02b252f383134
Alejandro <alejandrocumo18@gmail.com>
2023-01-14 19:06:18 -0300

    termine

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Naranja",
+      precio: 50,
+      img:
+        "./frutas/naranja.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Mandarina",
+      precio: 100,
+      img:
+        "./frutas/mandarina.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Uva",
+      precio: 150,
+      img:
+        "./frutas/uva.webp",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Coco",
+      precio: 200,
+      img:
+        "./frutas/coco.jpg",
+      cantidad: 1,
+    },
+  ];
